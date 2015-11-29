e9fc5eaee9778964837e3f7f33235ba0177394b1
flaminka <ewa.baranowska4@wp.pl>
2015-11-29 20:38:58 +0100

    zipy sa

--- beatbox.py
+++ beatbox.py
@@ -24,12 +24,47 @@ import zapis
 if __name__=='__main__':
     import sys
     # zapisuje nazwe folderu z trackami
-    utwor = sys.argv[1] 
+    argument = sys.argv[1] 
+    
+    import os    
+    import re
+    import zipfile
+   # ustawiamy sciezke dostepu do tmp 
+    folder_tmp = '/tmp/'    
+    
+    try: # gdy mamy utwor1.zip
+    #zakladam kodowanie 'latin-1' bo wtedy nie wywalalo mi bledu
+    #jesli chcialabym poszukac kodowania, to musialabym wziac liste wszystkich
+    #mozliwosci
+    #from encodings.aliases import aliases
+    # aliases.items()       
+    #i iterowac po nich forem z try-em, ale na razie naiwnie zakladam, ze to 
+    #co u mnie dziala, bedzie dzialac gdzie indziej
+        f = open(argument, 'r', encoding='latin-1') 
+        poczatek = f.read(10)
+        #sprawdzam czy zip poprzez magic number dla niego
+        if poczatek.startswith("\x50\x4b\x03\x04"):
+            zipek = zipfile.ZipFile(argument)
+            zipek.extractall(folder_tmp)
+            zipek.close()
+            nazwa_utworu = re.sub('.zip','',argument)
+            folder = ''.join([folder_tmp, nazwa_utworu, '/'])
+            os.chdir(folder)  
+            nazwa_pliku = ''.join([folder_tmp, nazwa_utworu, '.wav'])
+        f.close()
+    except UnicodeDecodeError:
+        print("Nie to kodowanie :/")
+    except IsADirectoryError: # gdy mamy utwor1/
+        folder = ''.join([os.getcwd(),'/',argument])
+        os.chdir(folder)
+        nazwa_pliku = ''.join([folder_tmp, re.sub('/','',argument), '.wav'])
+        
+        
+    
     
     # zmieniam sciezke workspace'a
-    import os
-    folder = ''.join([os.getcwd(),'/',utwor])
-    os.chdir(folder)
+    
+
     
     #wczytujemy tracki w jedna dluga macierz i zapisuje czy tracki byly ok
     macierz_song,ok = odczyt.wczytywanie_sciezek(odczyt.wczytywanie_piosenki())
@@ -44,12 +79,7 @@ if __name__=='__main__':
     import scipy.io.wavfile
     #import scipy.signal
     
-    # ustawiamy sciezke dostepu do tmp 
-    folder_tmp = '/tmp/'
-    import re
 
-    nazwa_pliku = ''.join([folder_tmp,re.sub('/','',utwor),'.wav'])  
-    
     # zapisujemy plik w folderze tmp pod odpowiednia nazwa
     scipy.io.wavfile.write(nazwa_pliku, 
                            44100,
